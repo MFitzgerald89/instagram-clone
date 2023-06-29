7b68725f1d3700d9976adef13d56698ef608cbb6
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-06-28 20:12:38 -0400

    adds nav, timeline, suggestions folders

---
+++ src/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="homepage__nav"></div>
+      <div className="home__timeline"></div>{" "}
+    </div>
+  );
+}
+export default Homepage;
